c733da8fa8b52745e9a5973a9136b39b49047e9c
Chengsheng Mao <chengsheng.mao@northwestern.edu>
2023-09-25 14:00:32 -0500

    update

--- maskrcnn_benchmark/data/datasets/cxr.py
+++ maskrcnn_benchmark/data/datasets/cxr.py
@@ -535,3 +535,268 @@ class Chexpert(VisionDataset):
                 'n_img': n_img, 'n_prompt': n_prompt, 'index':index, 'study_id': data['Path']}
 
 
+class Chexpert5x200(Chexpert):
+    """MimicCXR_V2 dataset imported from TorchVision.
+        It is modified to handle several image sources
+
+    Args:
+        root (string): Path to the dataset
+        metafile (string): Path to meta data.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(
+            self,
+            root: str,
+            metafile: str = 'candidate.csv',
+            hierarchy: bool = False,
+            use_PNUprompt: bool =False,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            transforms: Optional[Callable] = None,
+    ) -> None:
+        super(Chexpert5x200, self).__init__(root, metafile,hierarchy,use_PNUprompt, transform, target_transform, transforms)
+
+        self.label_prompt = pd.DataFrame ([  ['Disease Atelectasis is not found.', 'Disease Atelectasis is found.', 'Not sure if Disease Atelectasis is found.'],
+                ['Disease Cardiomegaly is not found.', 'Disease Cardiomegaly is found.', 'Not sure if Disease Cardiomegaly is found.'], 
+                ['Disease Consolidation is not found.', 'Disease Consolidation is found.', 'Not sure if Disease Consolidation is found.'], 
+                ['Disease Edema is not found.', 'Disease Edema is found.', 'Not sure if Disease Edema is found.'], 
+                ['Disease Pleural Effusion is not found.', 'Disease Pleural Effusion is found.', 'Not sure if Disease Pleural Effusion is found.'], 
+               ['Chest Disease is found.', 'Chest Disease is not found.', 'Not sure if Chest Disease is found.']],
+            index = ['Atelectasis','Cardiomegaly','Consolidation','Edema','Pleural Effusion','No Finding'],
+            columns = ['negative', 'positive', 'uncertain'])
+
+
+class Chexpert500(Chexpert):
+    """MimicCXR_V2 dataset imported from TorchVision.
+        It is modified to handle several image sources
+
+    Args:
+        root (string): Path to the dataset
+        metafile (string): Path to meta data.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(
+            self,
+            root: str,
+            metafile: str = 'groundtruth.csv',
+            hierarchy: bool = False,
+            use_PNUprompt: bool =False,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            transforms: Optional[Callable] = None,
+    ) -> None:
+        super(Chexpert500, self).__init__(root, metafile,hierarchy,use_PNUprompt, transform, target_transform, transforms)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        """
+        data = self.meta_data.iloc[index]
+        image_folder = data['Study']
+        image_files = glob.glob(os.path.join(self.root, image_folder)+'/*.jpg')
+        label = data.loc[self.label_prompt.index]
+        # present = label[label==1].index
+        # absent = label[label==0].index
+        # uncertain = label[label==2].index
+        # unmentioned = label[label.isna()].index
+        if not self.hierarchy:
+            label_prompt = []  
+            prompt_target = []
+            if not self.use_PNUprompt:        
+                for x, v in label.items():
+                    if v==1:
+                        label_prompt.append(self.label_prompt.loc[x, 'positive'])
+                    elif v==0:
+                        label_prompt.append(self.label_prompt.loc[x, 'negative'])
+                    elif v==2:
+                        label_prompt.append(self.label_prompt.loc[x, 'uncertain'])
+                    else:
+                        continue
+
+                if len(label_prompt)==0:
+                    label_prompt = ['Not sure if Chest Disease is found.']
+            else:
+                prompt_target = label.dropna()
+                label_prompt = self.label_prompt
+
+                if len(prompt_target)==0: 
+                    prompt_target = pd.Series({'No Finding':2.0})
+        else:
+            label_prompt = [[], []]  
+            prompt_target = [[], []]
+            if not self.use_PNUprompt:        
+                for x, v in label.items():
+                    if v==1:
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'positive'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'positive'])
+                    elif v==0 or pd.isna(v):
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'negative'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'negative'])
+                    elif v==2:
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'uncertain'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'uncertain'])
+                    else:
+                        pass
+            else:
+                label_prompt[0] = self.label_prompt.iloc[:-1]
+                label_prompt[1] = self.label_prompt.iloc[-1]
+                prompt_target[0] = label.drop('No Finding').fillna(0).values
+                prompt_target[1] = label['No Finding'] 
+
+        n_prompt = len(label_prompt) 
+
+        images = [Image.open(fn).convert("RGB") for fn in image_files]
+        if self.transforms is not None:
+            images = [self.transforms(img) for img in images]
+        n_img = len(images)
+
+        return {'images': images, 'text': '', 'label': label.values, 'label_prompt': label_prompt, 'prompt_target':prompt_target, 
+                'n_img': n_img, 'n_prompt': n_prompt, 'index':index, 'study_id': image_folder}
+
+
+class RSNA(VisionDataset):
+    """MimicCXR_V2 dataset imported from TorchVision.
+        It is modified to handle several image sources
+
+    Args:
+        root (string): Path to the dataset
+        metafile (string): Path to meta data.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(
+            self,
+            root: str,
+            hierarchy: bool = False,
+            use_PNUprompt: bool =False,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            transforms: Optional[Callable] = None,
+    ) -> None:
+        super(RSNA, self).__init__(root, transforms, transform, target_transform)
+
+        neg_images = glob.glob(os.path.join(root,'nopneumonia/*'))
+        pos_images = glob.glob(os.path.join(root,'pneumonia/*'))
+        self.meta_data = pd.DataFrame({'Pneumonia': [0]*len(neg_images)+[1]*len(pos_images), 'path': neg_images+pos_images})
+        self.meta_data['No Finding'] = 1-self.meta_data['Pneumonia']
+        self.all_meta_data = self.meta_data
+        self.label_prompt = pd.DataFrame ([  ['Disease Pneumonia is not found.', 'Disease Pneumonia is found.', 'Not sure if Disease Pneumonia is found.'], 
+               ['Chest Disease is found.', 'Chest Disease is not found.', 'Not sure if Chest Disease is found.']],
+            index = ['Pneumonia','No Finding'],
+            columns = ['negative', 'positive', 'uncertain'])
+
+        self.hierarchy = hierarchy
+        self.use_PNUprompt = use_PNUprompt
+
+
+    def __len__(self):
+        return len(self.meta_data)  
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        """
+        data = self.meta_data.iloc[index]
+        image_files = [data['path']]
+        label = data.loc[self.label_prompt.index]
+        # present = label[label==1].index
+        # absent = label[label==0].index
+        # uncertain = label[label==2].index
+        # unmentioned = label[label.isna()].index
+        if not self.hierarchy:
+            label_prompt = []  
+            prompt_target = []
+            if not self.use_PNUprompt:        
+                for x, v in label.items():
+                    if v==1:
+                        label_prompt.append(self.label_prompt.loc[x, 'positive'])
+                    elif v==0:
+                        label_prompt.append(self.label_prompt.loc[x, 'negative'])
+                    elif v==2:
+                        label_prompt.append(self.label_prompt.loc[x, 'uncertain'])
+                    else:
+                        continue
+
+                if len(label_prompt)==0:
+                    label_prompt = ['Not sure if Chest Disease is found.']
+            else:
+                prompt_target = label.dropna()
+                label_prompt = self.label_prompt
+
+                if len(prompt_target)==0: 
+                    prompt_target = pd.Series({'No Finding':2.0})
+        else:
+            label_prompt = [[], []]  
+            prompt_target = [[], []]
+            if not self.use_PNUprompt:        
+                for x, v in label.items():
+                    if v==1:
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'positive'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'positive'])
+                    elif v==0 or pd.isna(v):
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'negative'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'negative'])
+                    elif v==2:
+                        if x == 'No Finding':
+                            label_prompt[1].append(self.label_prompt.loc[x, 'uncertain'])
+                        else:
+                            label_prompt[0].append(self.label_prompt.loc[x, 'uncertain'])
+                    else:
+                        pass
+            else:
+                label_prompt[0] = self.label_prompt.iloc[:-1]
+                label_prompt[1] = self.label_prompt.iloc[-1]
+                prompt_target[0] = label.drop('No Finding').fillna(0).values
+                prompt_target[1] = label['No Finding'] 
+
+        n_prompt = len(label_prompt) 
+
+        images = [Image.open(fn).convert("RGB") for fn in image_files]
+        if self.transforms is not None:
+            images = [self.transforms(img) for img in images]
+        n_img = len(images)
+
+        return {'images': images, 'text': '', 'label': label.values, 'label_prompt': label_prompt, 'prompt_target':prompt_target, 
+                'n_img': n_img, 'n_prompt': n_prompt, 'index':index, 'study_id': data['path']}
+
+    def split(self):
+        n_all=len(self.all_meta_data) 
+        np.random.seed(0)
+
+        tr_df, val_df, te_df = np.split(self.all_meta_data.sample(frac=1, random_state=0),[int(n_all*0.6), int(n_all*0.8),])
+
+        tr_set, val_set, te_set = copy.copy(self), copy.copy(self), copy.copy(self)
+        tr_set.meta_data, val_set.meta_data, te_set.meta_data  = tr_df, val_df, te_df
+
+        return tr_set, val_set, te_set
